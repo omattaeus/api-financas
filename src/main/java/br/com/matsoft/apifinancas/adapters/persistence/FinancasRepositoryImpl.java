71d29774c9a8b06d0359b0257c64e6bdb90a7294
omattaeus <gomesduartedev@gmail.com>
2024-05-01 17:37:09 -0300

    feat: implementando a classe financasrepositoryimpl

--- src/main/java/br/com/matsoft/apifinancas/adapters/persistence/FinancasRepositoryImpl.java
+++ src/main/java/br/com/matsoft/apifinancas/adapters/persistence/FinancasRepositoryImpl.java
@@ -3,22 +3,35 @@ package br.com.matsoft.apifinancas.adapters.persistence;
 import br.com.matsoft.apifinancas.application.dtos.DespesasDTO;
 import br.com.matsoft.apifinancas.application.dtos.OrcamentosDTO;
 import br.com.matsoft.apifinancas.application.dtos.ReceitasDTO;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class FinancasRepositoryImpl implements FinancasRepository{
+
+    @Autowired
+    private final EntityManager entityManager;
+
+    public FinancasRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public DespesasDTO saveDespesas(DespesasDTO despesas) {
-        return null;
+        entityManager.persist(despesas);
+        return despesas;
     }
 
     @Override
     public OrcamentosDTO saveOrcamentos(OrcamentosDTO orcamentos) {
-        return null;
+        entityManager.persist(orcamentos);
+        return orcamentos;
     }
 
     @Override
     public ReceitasDTO saveReceitas(ReceitasDTO receitas) {
-        return null;
+        entityManager.persist(receitas);
+        return receitas;
     }
 }
